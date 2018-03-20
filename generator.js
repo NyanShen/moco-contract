80db95872df2ba4b1eafaa33cb8e50304c5e2cf8
nyan <sibaoemail@126.com>
2018-03-20 20:44:05 +0800

    update

--- generator.js
+++ generator.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-var result=[];//字典
+var result=[];
 
 var readDir = function(dir){
     var files = fs.readdirSync(dir);
     files.forEach(function(file){
         var stats = fs.statSync(dir+'/'+file);
         if(stats.isFile()&&file.indexOf(".json")!==-1){
-            result.push('include:'+dir+'/'+file);
+            result.push({include:+dir+'/'+file});
         }
         if(stats.isDirectory()){
             readDir(dir+'/'+file);
