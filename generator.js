1e1086f3efd1e9b09ad20328d65bcd3f2f6fc5b6
nyan <sibaoemail@126.com>
2018-03-24 12:03:00 +0800

    update moco

--- generator.js
+++ generator.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-var result=[];
+var result=[];//字典
 
 var readDir = function(dir){
     var files = fs.readdirSync(dir);
     files.forEach(function(file){
         var stats = fs.statSync(dir+'/'+file);
-        if(stats.isFile()&&file.indexOf(".json")!==-1){
-            result.push({include:+dir+'/'+file});
+        if(stats.isFile()&&file.indexOf(".json")!=-1){
+            result.push({include: dir+'/'+file});
         }
         if(stats.isDirectory()){
             readDir(dir+'/'+file);
         }
     });
-};
-readDir('react-api');
+}
+readDir('api');
 fs.writeFileSync('api.json',JSON.stringify(result,null,2));
