08b7b8fd897c6f4d53f6f26e5e43c984604753f2
NyanShen <3354509516@qq.com>
2018-06-25 15:15:56 +0800

    add tokens git ignore

--- generator.js
+++ generator.js
@@ -7,13 +7,13 @@ var readDir = function(dir){
     var files = fs.readdirSync(dir);
     files.forEach(function(file){
         var stats = fs.statSync(dir+'/'+file);
-        if(stats.isFile()&&file.indexOf(".json")!=-1){
+        if(stats.isFile()&&file.indexOf(".json")!==-1){
             result.push({include: dir+'/'+file});
         }
         if(stats.isDirectory()){
             readDir(dir+'/'+file);
         }
     });
-}
+};
 readDir('api');
 fs.writeFileSync('api.json',JSON.stringify(result,null,2));
